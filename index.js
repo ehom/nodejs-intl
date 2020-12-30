beced5a8bafbcdb4ca98b975a3294235d144645f
ehom <ehom@acm.org>
2020-12-29 22:49:32 -0800

    This tool writes out the list of a supported intl objects for the nodejs version.

---
+++ index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+// get List of Intl objects supported
+const supportedObjects = new Set(Object.getOwnPropertyNames(Intl));
+
+let tableOfNames = {
+  'DateTimeFormat': false,
+  'RelativeTimeFormat': false,
+  'NumberFormat': false,
+  'Collator': false,
+  'PluralRules': false,
+  'ListFormat': false,
+  'Locale': false,
+  'getCanonicalLocales': false
+};
+
+// console.debug(tableOfNames);
+// console.debug(supportedObjects);
+
+for (const name of Object.keys(tableOfNames)) {
+  tableOfNames[name] = supportedObjects.has(name);
+}
+
+tableOfNames['Node.js'] = {version: process.version};
+
+console.debug(tableOfNames);
+
+const content = JSON.stringify(tableOfNames, null, 2);
+
+fs.writeFileSync('./nodejs.json', content, 'utf-8');
